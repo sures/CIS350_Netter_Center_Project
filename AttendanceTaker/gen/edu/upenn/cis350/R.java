8664488c82d8a864001429dc56b8fcdd47ecb8b1
wellecks <wellecks@gmail.com>
2012-02-24 10:55:17 -0500

    added roster page with basic functionality

--- AttendanceTaker/gen/edu/upenn/cis350/R.java
+++ AttendanceTaker/gen/edu/upenn/cis350/R.java
@@ -19,15 +19,18 @@ public final class R {
     public static final class id {
         public static final int Button01=0x7f060003;
         public static final int Button02=0x7f060004;
+        public static final int MyTopButton=0x7f060005;
         public static final int button1=0x7f060000;
+        public static final int button2=0x7f060006;
         public static final int password=0x7f060002;
         public static final int username=0x7f060001;
     }
     public static final class layout {
         public static final int activity=0x7f030000;
         public static final int attendance=0x7f030001;
-        public static final int main=0x7f030002;
-        public static final int roster=0x7f030003;
+        public static final int list_item=0x7f030002;
+        public static final int main=0x7f030003;
+        public static final int roster=0x7f030004;
     }
     public static final class string {
         public static final int AddActivity=0x7f040007;
--- AttendanceTaker/src/edu/upenn/cis350/Roster.java
+++ AttendanceTaker/src/edu/upenn/cis350/Roster.java
@@ -1,17 +1,44 @@
 package edu.upenn.cis350;
-
-import android.app.Activity;
+import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class Roster extends Activity{
+/**
+ * represents the 'roster' page.
+ * extends ListActivity. Each list item is defined in list_item.xml, 
+ * while the overall layout is defined in roster.xml
+ *
+ * TODO: 	-put a selector / button in each list item to select present/absent
+ * 			-when clicked, each list item should go to corresponding profile
+ * 			-pull names from database rather than from static array
+ */
+public class Roster extends ListActivity{
 
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.roster);
+        //ListView stuff
+        setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, PEOPLE));
+        ListView lv = getListView();
+        lv.setTextFilterEnabled(true);
+        
+        //click listener for each list item
+        lv.setOnItemClickListener(new OnItemClickListener() {
+          public void onItemClick(AdapterView<?> parent, View view,
+              int position, long id) {
+            // When clicked, show a toast with the TextView text
+            Toast.makeText(getApplicationContext(), "Present",
+                Toast.LENGTH_SHORT).show();
+          }
+        });
     }
 	
 	public void onBackButtonClick(View view) {
@@ -19,5 +46,16 @@ public class Roster extends Activity{
 		setResult(RESULT_OK, i);
 		finish();	
 	}
+	public void onSubmitButtonClick(View view) {
+		Toast.makeText(getApplicationContext(), "Attendance Submitted",
+                Toast.LENGTH_SHORT).show();
+	}
 	
+	//array holding data items for the list (temporary)
+	//in future will pull these from a database or spreadsheet
+	static final String[] PEOPLE = new String[] {
+	    "Alice J.", "Bob D.", "Catherine H.", "Christian C.", "Victor G.",
+	    "Rupi S.", "Sean W.","Alice J.", "Bob D.", "Catherine H.", "Christian C.", "Victor G.",
+	    "Rupi S.", "Sean W."
+	  };
 }
